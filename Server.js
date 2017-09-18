931c717219d6aebc1841e8c602591c3202f3cfa4
sadrltd <31811037+sadrltd@users.noreply.github.com>
2017-09-18 02:19:35 +0200

    Update Server.js

--- Server.js
+++ Server.js
@@ -9,6 +9,7 @@ logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
 });
+
 function connectToServer() {                            // Called on a user running !command
     return new Promise( function(resolve, reject) {     // Returns a promise. This basically says, wait a sec and I'll give you what you want
         http.request( {                                 // Make the request
@@ -21,13 +22,12 @@ function connectToServer() {                            // Called on a user runn
                 "Content-Type": "application/x-www-form-urlencoded",
                 "Connection": "keep-alive"
             }
-    }, function (response) {                // Then, when we recieve a response
-            response.on("end", () => {      // And have finished recieving data
-                resolve()                   // Resolve the promise. This runs the code inside connectToServer.then(function() { code here }
-            };
-                        })
-            
-}
+    }, 
+		function (response) {                // Then, when we recieve a response
+			response.on("end", () => {      // And have finished recieving data
+				resolve()                   // Resolve the promise. This runs the code inside connectToServer.then(function() { code here }
+            })
+    });
                      
 var http = new XMLHttpRequest();
 var url = "http://p2.zgirls.games:8081/gameservice/loading_record.php?type=2&action=kingdom.officers&gameUid=444473466000093&server=59&appVersion=1.0.63";
@@ -36,8 +36,7 @@ http.open("POST", url, true);
 
 http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         
-function httpGetAsync(Url, callback)
-{
+function httpGetAsync(Url, callback){
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
@@ -46,7 +45,6 @@ function httpGetAsync(Url, callback)
     xmlHttp.open("GET", Url, true); // true for asynchronous 
     xmlHttp.send(null);
 }
-
                      
 logger.level = 'debug';
 // Initialize Discord Bot
@@ -58,28 +56,18 @@ var bot = new Discord.Client({
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info(bot.username + ' - (' + bot.id + ')')
 });
+
         
 bot.on("message", function(msg) {               // On any message anywhere
-    if (msg.content == "!connect") {            // If the message is !connect
-        connectToServer().then(function() {     // Make the request. When teh request finishes: 
+    if (msg.content == "*connect") {            // If the message is !connect
+        connectToServer().then(function() {     // Make the request. When the request finishes: 
             message.reply("Connected! ")            // Reply
-        });
-        
+});
+        		
 bot.on("message", function(msg) {               
-    if (msg.content == "!king") {            
+    if (msg.content == "*king") {            
         httpGetAsync().then(function() {     
             message.reply("Test! ")            // Reply
-        });        
-        
-    }
-})
-  
-console.log("---------------------------");
-console.log("Cant meow like me. ");
-console.log("Listening on port " + port);
-console.log("---------------------------");
-
-
-
+});
